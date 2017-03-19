d7847a915fa28afd50f717139f491256d0a8b729
Do Gia Dat <dogiadat@gmail.com>
2017-03-19 15:09:19 +0700

    user login/logout

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Easy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Hanoi"
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/**/*.{rb,yml}"]
+    config.i18n.default_locale = :vi
   end
 end
