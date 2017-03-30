4c6bdf98b10aae806a7d1cbb4a4010972acf47da
Do Gia Dat <dogiadat@gmail.com>
2017-03-30 11:06:02 +0700

    edit restaurant

--- app/assets/javascripts/admin/map.js
+++ app/assets/javascripts/admin/map.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
+  var lat = 21.0167326
+  var long = 105.78417680000007
+  lat = $('#restaurant-lat').val() == '' ? lat : parseFloat($('#restaurant-lat').val())
+  long = $('#restaurant-long').val() == '' ? long : parseFloat($('#restaurant-long').val())
   $('#restaurant-map').locationpicker({
+    location: {latitude: lat, longitude: long},
     inputBinding: {
       latitudeInput: $('#restaurant-lat'),
       longitudeInput: $('#restaurant-long'),
--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -7,15 +7,31 @@ class Admin::RestaurantsController < Admin::BaseController
   def new
   end
 
+  def edit
+  end
+
   def create
     if @restaurant.save
       params[:restaurant_categories].each do |category_id|
         @restaurant.restaurant_categories.create category_id: category_id
       end
       flash[:success] = "Nhà hàng đưọc thêm mới thành công"
-      redirect_to admin_categories_path
+      redirect_to admin_restaurants_path
     else
       render :new
+    end
+  end
+
+  def update
+    if @restaurant.save
+      @restaurant.restaurant_categories.where.not(category_id: params[:restaurant_categories]).delete_all
+      params[:restaurant_categories].each do |category_id|
+        @restaurant.restaurant_categories.find_or_create_by category_id: category_id
+      end
+      flash[:success] = "Nhà hàng đưọc thêm cập nhật thành công"
+      redirect_to admin_restaurants_path
+    else
+      render :edit
     end
   end
 
